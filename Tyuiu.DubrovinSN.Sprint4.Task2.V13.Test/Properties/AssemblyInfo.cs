89deffd9e3f9a9d6262ac6bb8e6147ff3e60cb27
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-10-30 16:18:42 +0500

    Task2

---
+++ Tyuiu.DubrovinSN.Sprint4.Task2.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DubrovinSN.Sprint4.Task2.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DubrovinSN.Sprint4.Task2.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec12fb39-67e8-441d-8ee5-b424d6796073")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
